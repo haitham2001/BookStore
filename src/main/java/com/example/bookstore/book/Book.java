3c3185e19457adf50b115eb1f8f696be53c071f7
haitham2001 <m.haitham2001@gmail.com>
2024-04-23 23:31:24 +0200

    First commit

---
+++ src/main/java/com/example/bookstore/book/Book.java
@@ -0,0 +1,9 @@
+package com.example.bookstore.book;
+
+public class Book {
+
+    private String title;
+    private String author;
+    private String ispn;
+    private Integer publicationYear;
+}
